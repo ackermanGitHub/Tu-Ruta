6cfb70223be491cfc9baeb1e57b44efca087ab3c
ackerman <julio.sergio2709@gmail.com>
2023-07-11 01:50:35 -0400

    🔧⚙️: why were this useEffect here anyway?

--- apps/expo/src/app/_layout.tsx
+++ apps/expo/src/app/_layout.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState } from "react";
+import React from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { ClerkProvider/* , SignedIn, SignedOut */ } from "@clerk/clerk-expo";
 import { tokenCache } from "../utils/cache";
@@ -13,45 +13,14 @@ import { useFonts } from 'expo-font';
 void SplashScreen.preventAutoHideAsync();
 
 const RootLayout = () => {
-  const [appIsReady, setAppIsReady] = useState(false);
-  const [fontsLoaded] = useFonts({
+
+  const [_fontsLoaded] = useFonts({
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
     'Inter-Regular': require('../../assets/Inter-Regular.otf'),
   });
   // const { colorScheme } = useColorScheme()
 
   // void NavigationBar.setBackgroundColorAsync(colorScheme === 'dark' ? 'black' : 'white')
-
-  useEffect(() => {
-    async function prepare() {
-      try {
-        // make any API calls you need to do here
-
-        await new Promise(resolve => setTimeout(resolve, 2000));
-      } catch (e) {
-        console.warn(e);
-      } finally {
-        // Tell the application to render
-        setAppIsReady(true);
-      }
-    }
-
-    void prepare();
-  }, []);
-
-  const onLayoutRootView = useCallback(async () => {
-    if (appIsReady) {
-      // This tells the splash screen to hide immediately! If we call this after
-      // `setAppIsReady`, then we may see a blank screen while the app is
-      // loading its initial state and rendering its first pixels. So instead,
-      // we hide the splash screen once we know the root view has already
-      // performed layout.
-      await SplashScreen.hideAsync();
-    }
-  }, [appIsReady]);
-
-  if (!appIsReady) {
-    return null;
-  }
 
   return (
     <ClerkProvider
@@ -59,7 +28,7 @@ const RootLayout = () => {
       tokenCache={tokenCache}
     >
 
-      <SafeAreaProvider onLayout={void onLayoutRootView}>
+      <SafeAreaProvider >
 
         <StatusBar />
         <Stack
